38565b77607d60883f3fdf275644f242a4af7ab1
DonghwanL <ehwan007@gmail.com>
2021-03-12 16:26:04 +0900

    [수정] Coordinates CSS / JS

--- Browser/Chapter 1/coordinates/main.js
+++ Browser/Chapter 1/coordinates/main.js
@@ -3,17 +3,19 @@ const horizontal = document.querySelector('.horizontal');
 const target = document.querySelector('.target');
 const tag = document.querySelector('.tag');
 
-document.addEventListener('mousemove', (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
+window.addEventListener('load', () => {
+    const targetRect = target.getBoundingClientRect();
+    const targetHalfWidth = targetRect.width / 2;
+    const targetHalfHeight = targetRect.height / 2;
 
-    console.log(`${x}, ${y}`);
+    document.addEventListener('mousemove', (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
 
-    vertical.style.left = `${x}px`;
-    horizontal.style.top = `${y}px`;
-    target.style.left = `${x}px`;
-    target.style.top = `${y}px`;
-    tag.style.top = `${y}px`;
-    tag.style.left = `${x}px`;
-    tag.innerHTML = `${x}px, ${y}px`;
+        vertical.style.transform = `translateX(${x}px)`;
+        horizontal.style.transform = `translateY(${y}px)`;
+        target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+        tag.style.transform = `translate(${x}px, ${y}px)`;
+        tag.innerHTML = `${x}px, ${y}px`;
+    });
 });
