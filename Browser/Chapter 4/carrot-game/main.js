32beb50ba1c8ef62c2c299943d4f5b62b49a28f1
DonghwanL <ehwan007@gmail.com>
2021-03-17 13:01:49 +0900

    [수정] 게임 정지 및 종료 기능 추가

--- Browser/Chapter 4/carrot-game/main.js
+++ Browser/Chapter 4/carrot-game/main.js
@@ -11,9 +11,15 @@ const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
+const popUp = document.querySelector('.pop-up');
+const popUpText = document.querySelector('.pop-up__message');
+const popUpRefresh = document.querySelector('.pop-up__refresh');
+
 let started = false;
 let score = 0;
 let timer = undefined;
+
+field.addEventListener('click', onFieldClick);
 
 gameBtn.addEventListener('click', () => {
     if (started) {
@@ -21,23 +27,42 @@ gameBtn.addEventListener('click', () => {
     } else {
         startGame();
     }
+});
 
-    started = !started;
+popUpRefresh.addEventListener('click', () => {
+    startGame();
+    hidePopUp();
 });
 
 function startGame() {
+    started = true;
     initGame();
     showStopButton();
     showTimerAndScore();
     startGameTimer();
 }
 
-function stopGame() {}
+function stopGame() {
+    started = false;
+    stopGameTimer();
+    hideGameButton();
+    showPopUpWithText('REPLAY❓');
+}
+
+function finishGame(win) {
+    started = false;
+    hideGameButton();
+    showPopUpWithText(win? 'YOU WON 🎉' : 'YOU LOST 💩')
+}
 
 function showStopButton() {
-    const icon = gameBtn.querySelector('.fa-play');
+    const icon = gameBtn.querySelector('.fas');
     icon.classList.add('fa-stop');
     icon.classList.remove('fa-play');
+}
+
+function hideGameButton() {
+    gameBtn.style.visibility = 'hidden';
 }
 
 function showTimerAndScore() {
@@ -53,17 +78,58 @@ function startGameTimer() {
     timer = setInterval(() => {
         if (remainingTimeSec <= 0) {
             clearInterval(timer);
+            finishGame(CARROT_COUNT === score);
             return;
         } 
-        
+
         updateTimerText(--remainingTimeSec);
     }, 1000); 
+}
+
+function stopGameTimer() {
+    clearInterval(timer);
 }
 
 function updateTimerText(time) {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60; 
     gameTimer.innerText = `${minutes}:${seconds}`
+}
+
+function showPopUpWithText(text) {
+    popUpText.innerText = text;
+    popUp.classList.remove('pop-up--hide');
+}
+
+function hidePopUp() {
+    popUp.classList.add('pop-up--hide');
+}
+
+function onFieldClick(event) {
+    if (!started) {
+        return;
+    }
+
+    const target = event.target;
+    
+    if (target.matches('.carrot')) {
+        // 당근!
+        target.remove();
+        score++;
+        updateScoreBoard();
+
+        if (score === CARROT_COUNT) {
+            finishGame(true);
+        }
+
+    } else if (target.matches('.bug')) {
+        stopGameTimer();
+        finishGame(false);
+    }
+}
+
+function updateScoreBoard() {
+    gameScore.innerText = CARROT_COUNT - score;
 }
 
 function initGame() {
