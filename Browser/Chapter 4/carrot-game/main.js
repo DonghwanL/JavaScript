4c30ed828542e8e46cdaa35f560bc0efb3145bf0
DonghwanL <ehwan007@gmail.com>
2021-03-17 15:24:23 +0900

    [수정] 게임 마무리, BGM 추가

--- Browser/Chapter 4/carrot-game/main.js
+++ Browser/Chapter 4/carrot-game/main.js
@@ -15,6 +15,12 @@ const popUp = document.querySelector('.pop-up');
 const popUpText = document.querySelector('.pop-up__message');
 const popUpRefresh = document.querySelector('.pop-up__refresh');
 
+const carrotSound = new Audio('./sound/carrot_pull.mp3');
+const alertSound = new Audio('./sound/alert.wav');
+const bgSound = new Audio('./sound/bg.mp3');
+const bugSound = new Audio('./sound/bug_pull.mp3');
+const winSound = new Audio('./sound/game_win.mp3');
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -40,6 +46,7 @@ function startGame() {
     showStopButton();
     showTimerAndScore();
     startGameTimer();
+    playSound(bgSound);
 }
 
 function stopGame() {
@@ -47,11 +54,26 @@ function stopGame() {
     stopGameTimer();
     hideGameButton();
     showPopUpWithText('REPLAY❓');
+    playSound(alertSound);
+    stopSound(bgsound);
 }
+
+function stopSound(sound) {
+    sound.pause();
+} 
 
 function finishGame(win) {
     started = false;
     hideGameButton();
+
+    if (win) {
+        playSound(winSound);
+    } else {
+        playSound(bugSound);
+    }
+
+    stopGameTimer();
+    stopSound(bgSound);
     showPopUpWithText(win? 'YOU WON 🎉' : 'YOU LOST 💩')
 }
 
@@ -59,6 +81,7 @@ function showStopButton() {
     const icon = gameBtn.querySelector('.fas');
     icon.classList.add('fa-stop');
     icon.classList.remove('fa-play');
+    gameBtn.style.visibility = 'visible';
 }
 
 function hideGameButton() {
@@ -116,6 +139,7 @@ function onFieldClick(event) {
         // 당근!
         target.remove();
         score++;
+        playSound(carrotSound);
         updateScoreBoard();
 
         if (score === CARROT_COUNT) {
@@ -128,11 +152,17 @@ function onFieldClick(event) {
     }
 }
 
+function playSound(sound) {
+    sound.currentTime = 0;
+    sound.play();
+}
+
 function updateScoreBoard() {
     gameScore.innerText = CARROT_COUNT - score;
 }
 
 function initGame() {
+    score = 0;
     field.innerHTML = '';
     gameScore.innerText = CARROT_COUNT;
     // 벌레와 당근을 생성, field에 추가
